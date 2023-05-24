317f2f8af5a97b93d6ee7931b2db65cd303f3f05
Ian Chege <ianchege001@gmail.com>
2023-05-25 00:33:52 +0300

    Implemented Button with Icon

--- src/components/SignInWithProvider.tsx
+++ src/components/SignInWithProvider.tsx
@@ -3,22 +3,15 @@ import { Button, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
 import Image from 'next/image'
+import { FcGoogle } from 'react-icons/fc'
 
 const SignInWithProvider: React.FC = () => {
   const [signInWithGoogle, user, loading, fbError] = useSignInWithGoogle(auth)
 
   return (
     <Flex>
-      <Button isLoading={loading} onClick={() => signInWithGoogle()}>
-        Continue with{' '}
-          <span style={{ marginLeft: '8px' }}>
-            <Image
-              src="/images/google-logo.png"
-              alt="Google"
-              height={20}
-              width={20}
-            />
-          </span>
+      <Button rightIcon={<FcGoogle/>} isLoading={loading} loadingText='Loading' onClick={() => signInWithGoogle()}>
+        Continue with 
       </Button>
       {fbError && (
         <Text textAlign="center" color="red" fontSize="10pt">
